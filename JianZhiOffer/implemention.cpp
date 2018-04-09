09ac693a92161ec94f8ddd81cb364830e4862c60
purgle10 <feiliochen@gmail.com>
2018-04-09 11:32:26 +0800

    quiz9

--- JianZhiOffer/implemention.cpp
+++ JianZhiOffer/implemention.cpp
@@ -1,10 +1,39 @@
-#include<iostream>
-
-using namespace std;
-
-int main(){
-	int a;
-	cin >> a;
-	cout << a;
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//struct BinaryTreeNode{
+//	int m_nValue;
+//	BinaryTreeNode* m_pLeft;
+//	BinaryTreeNode* m_pRight;
+//};
+//
+//void preReverse(BinaryTreeNode *node){
+//	if (node == nullptr)
+//		return ;
+//	cout << node->m_nValue << endl;
+//	preReverse(node->m_pLeft);
+//	preReverse(node->m_pRight);
+//}
+//
+//void create(BinaryTreeNode* &node){
+//	char value;
+//	cin >> value;
+//	if (value == '#'){
+//		node = nullptr;
+//	}
+//	else{
+//		node = new BinaryTreeNode();
+//		node->m_nValue = int(value-'0');
+//		create(node->m_pLeft);
+//		create(node->m_pRight);
+//	}
+//	return;
+//}
+//
+//int main(){
+//	BinaryTreeNode *node = new BinaryTreeNode();
+//	create(node);
+//	preReverse(node);
+//}
