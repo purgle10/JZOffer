70b7bfec329f764947a75a11d62b2f6792f8f651
purgle10 <feiliochen@gmail.com>
2018-04-09 11:33:10 +0800

    quiz9

---
+++ JianZhiOffer/quiz9.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+template<typename type>
+class Queue{
+private:
+	stack<type> st1;
+	stack<type> st2;
+public:
+	void appendTail(type add);
+	type deleteHead();
+	Queue() :st1(), st2(){};
+	~Queue(){};
+};
+
+template<typename type>
+void Queue::appendTail(type add){
+	st1.push(add);
+}
+
+template<typename type>
+type Queue::deleteHead(){
+	if (st1.empty())
+		throw new std::exception("queue is empty.");
+	while (!st1.empty())
+	{
+		st2.push(st1.top());
+		st1.pop();
+	}
+	type Head = st2.top();
+	st2.pop();
+	while (!st2.empty()){
+		st1.push(st2.top());
+		st2.pop();
+	}
+	return Head;
+}
+
+int main(){
+
+}
