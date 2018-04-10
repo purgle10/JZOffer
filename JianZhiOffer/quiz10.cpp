4b77e60badf975dca2e902cd482c7b4730a0b119
purgle10 <feiliochen@gmail.com>
2018-04-10 17:21:28 +0800

    quiz10

--- JianZhiOffer/quiz10.cpp
+++ JianZhiOffer/quiz10.cpp
@@ -2,44 +2,42 @@
 
 using namespace std;
 
-long long Fibonacci(long long n){
+long long Fibonacci(unsigned int n, long long fibNum1 = 0, long long fibNum2 = 1){
 	if (n <= 0)
 		return 0;
 	if (n == 1)
-		return 1;
+		return fibNum2;
 	return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
-long long FibonacciAnother( unsigned int n, long long fibNum = 1, long long fibNum1 = 0){
+long long FibonacciAnother( unsigned int n, long long fibNum1, long long fibNum2){
 	if (n <= 0)
 		return 0;
 	if (n == 1)
-		return fibNum;
-	return FibonacciAnother(n-1, fibNum + fibNum1, fibNum);
+		return fibNum2;
+	return FibonacciAnother(n - 1, fibNum2, fibNum2 + fibNum1);
 }
 
-long long FibonacciLoop(unsigned int n){
-	long long fibNum1 = 0;
-	long long fibNum2 = 1;
+long long FibonacciLoop(unsigned int n, long long fibNum1, long long fibNum2){
 	long long fibNum = 0;
 	if (n <= 0)
 		return 0;
 	if (n == 1)
 		return fibNum2;
-	for (int i = 1; i < n; ++i){
+	for (unsigned int i = 1; i < n; ++i){
 		fibNum = fibNum1 + fibNum2;
 		fibNum1 = fibNum2;
 		fibNum2 = fibNum;
-		//fibNum2 = fibNum1 + fibNum2;
 	}
 	return fibNum;
 }
 
 int main(){
 	int n;
+	long long fibNum1 = 0, fibNum2 = 1;
 	cin >> n;
-	//cout << Fibonacci(n) << endl;
-	cout << FibonacciAnother(n) << endl;
-	cout << FibonacciLoop(n) << endl;
+	cout << Fibonacci(n) << endl;
+	cout << FibonacciAnother(n, fibNum1, fibNum2) << endl;
+	cout << FibonacciLoop(n, fibNum1, fibNum2) << endl;
 	return 0;
 }
