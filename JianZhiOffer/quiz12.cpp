347fc9c7fb843028e46b3b45c6536e28be39f3f5
purgle10 <feiliochen@gmail.com>
2018-04-11 14:41:00 +0800

    backtracking

---
+++ JianZhiOffer/quiz12.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int count = 0;
+int rowM = 0;
+int colM = 0;
+
+bool check(vector<char> strM, int row, int col, string str, int index, vector<bool> useFlag){
+	if (row < 0 || row > rowM - 1 || col < 0 || col > colM - 1 || useFlag[row*col+col])
+		return false;
+	if (index == str.length())
+		return true;
+	useFlag[row*col + col] = true;
+	if (str[index] != strM[row*colM + col]) //colM instead of col
+		return false;
+
+	bool isTrue = check(strM, row - 1, col, str, index + 1, useFlag) || check(strM, row + 1, col, str, index + 1, useFlag) || check(strM, row, col - 1, str, index + 1, useFlag) || \
+		check(strM, row, col + 1, str, index + 1, useFlag);
+	if (!isTrue){
+		--index;
+		useFlag[row*col + col] = false;
+	}
+	return isTrue;
+}
+
+int main(){
+	//int row, col;
+	vector<char> strMatrix;
+	cin >> rowM >> colM;
+	for (int i = 0; i < rowM*colM; ++i){
+		char input;
+		cin >> input;
+		strMatrix.push_back(input);
+	}
+	string str;
+	cin >> str;
+
+	vector<bool> useFlag(rowM*colM);
+	bool exist = false;
+	for (int i = 0; i < rowM; ++i){
+		for (int j = 0; j < colM; ++j){
+			if (check(strMatrix, i, j, str, 0, useFlag)){
+				exist = true;
+				cout << "True" << endl;
+				break;
+			}
+		}
+	}
+	if (!exist)
+		cout << "False" << endl;
+	return 0;
+}
